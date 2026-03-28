69a3119bf58784d57115c072c19e56d190f6ef21
NarmadhaAnnadurai <narmadha.mphil14@gmail.com>
2026-03-28 15:58:45 +0530

    Add files via upload

--- Simulated Results/UACI.m
+++ Simulated Results/UACI.m
@@ -1,1 +1,26 @@
+clc; clear; close all;
 
+% Read Original and Modified Encrypted Images
+originalImage = imread('/MATLAB Drive/Final Permuted Images/xraypermuted_image.png'); % First encrypted image
+modifiedImage = imread('/MATLAB Drive/Final Encrypted Images/xraypermutedbmke_oneencrypted.png'); % Second encrypted image (with one pixel change)
+
+% Convert to grayscale if needed
+if size(originalImage,3) == 3
+    originalImage = rgb2gray(originalImage);
+end
+if size(modifiedImage,3) == 3
+    modifiedImage = rgb2gray(modifiedImage);
+end
+
+% Convert images to double for calculation
+originalImage = double(originalImage);
+modifiedImage = double(modifiedImage);
+
+% Get Image Size
+[M, N] = size(originalImage);
+
+% Compute UACI
+UACI = (sum(abs(originalImage - modifiedImage), 'all') / (M * N * 255)) * 100;
+
+% Display Result
+fprintf('UACI Value: %.2f%%\n', UACI);
